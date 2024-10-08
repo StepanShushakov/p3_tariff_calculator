3b1944f4a685f8246ed85965c1ae291015ff354c
Stepan Shushakov <stepan.shushakov@astorsoft.ru>
2024-10-08 17:15:05 +0700

    change transfer cost with distance long

--- app/src/main/java/ru/fastdelivery/config/Beans.java
+++ app/src/main/java/ru/fastdelivery/config/Beans.java
@@ -2,6 +2,7 @@ package ru.fastdelivery.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Qualifier;
 import ru.fastdelivery.domain.common.currency.CurrencyFactory;
 import ru.fastdelivery.domain.common.currency.CurrencyPropertiesProvider;
 import ru.fastdelivery.usecase.CoordinatesBorders;
@@ -20,7 +21,8 @@ public class Beans {
     }
 
     @Bean
-    public TariffCalculateUseCase tariffCalculateUseCase(WeightPriceProvider weightPriceProvider, CoordinatesBorders coordinatesBorders) {
+    public TariffCalculateUseCase tariffCalculateUseCase(WeightPriceProvider weightPriceProvider,
+                                                         @Qualifier("coordinatesProperties") CoordinatesBorders coordinatesBorders) {
         return new TariffCalculateUseCase(weightPriceProvider, coordinatesBorders);
     }
 }
--- useCase/src/main/java/ru/fastdelivery/usecase/TariffCalculateUseCase.java
+++ useCase/src/main/java/ru/fastdelivery/usecase/TariffCalculateUseCase.java
@@ -6,6 +6,7 @@ import ru.fastdelivery.domain.delivery.shipment.Point;
 import ru.fastdelivery.domain.delivery.shipment.Shipment;
 
 import javax.inject.Named;
+import java.math.BigDecimal;
 
 @Named
 @RequiredArgsConstructor
@@ -25,11 +26,19 @@ public class TariffCalculateUseCase {
                 .costPerCubicMeter()
                 .multiply(volumeAllPackagesM3);
 
-        return weightPriceProvider
+        Price basePrice = weightPriceProvider
                 .costPerKg()
                 .multiply(weightAllPackagesKg)
                 .max(pricePerVolume)
                 .max(minimalPrice);
+
+        double distance = calculateDistance(destination, departure) / 1000;
+
+        if (distance < 450) {
+            return basePrice;
+        }
+
+        return new Price(BigDecimal.valueOf(distance / 450).multiply(basePrice.amount()), basePrice.currency());
     }
 
     private void checkCoordinates(Point destination) {
@@ -46,5 +55,22 @@ public class TariffCalculateUseCase {
 
     public Price minimalPrice() {
         return weightPriceProvider.minimalPrice();
+    }
+
+    public static Double calculateDistance(Point point1, Point point2) {
+        // Радиус Земли в метрах
+        final double EARTH_RADIUS = 6372795;
+
+        // Преобразование градусов в радианы
+        double lat1Rad = Math.toRadians(point1.latitude().value());
+        double lat2Rad = Math.toRadians(point2.latitude().value());
+        double long1Rad = Math.toRadians(point1.longitude().value());
+        double long2Rad = Math.toRadians(point2.longitude().value());
+
+        // Разница долгот
+        double deltaLon = long2Rad - long1Rad;
+
+        return Math.acos(Math.sin(lat1Rad) * Math.sin(lat2Rad) +
+                Math.cos(lat1Rad) * Math.cos(lat2Rad) * Math.cos(deltaLon)) * EARTH_RADIUS;
     }
 }
